71446ebe9e2868fe88f6e0456f01d12957053c4b
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-02-09 10:35:10 +0100

    init

---
+++ src/pick-color.ts
@@ -0,0 +1,22 @@
+import { Clipboard, closeMainWindow, showHUD } from "@raycast/api";
+import { toTailwind, pickColor } from "./utils";
+
+export default async function command() {
+  await closeMainWindow();
+
+  try {
+    const pickedColor = await pickColor();
+    if (!pickedColor) {
+      return;
+    }
+
+    const tw = toTailwind(pickedColor);
+
+    await Clipboard.copy(tw);
+    await showHUD(`✅ Copied ${tw} to clipboard!`);
+  } catch (e) {
+    console.error(e);
+
+    await showHUD("❌ Failed picking color");
+  }
+}
---
+++ src/utils.ts
@@ -0,0 +1,71 @@
+import { environment } from "@raycast/api";
+import { execa, ExecaError } from "execa";
+import { chmod } from "fs/promises";
+import { join } from "path";
+import { getTransformedTailwindConfig } from "./tailwind";
+
+export type RGB = Record<"r" | "g" | "b", number>;
+
+export async function pickColor() {
+  const command = join(environment.assetsPath, "color-picker");
+  await chmod(command, 0o755);
+
+  try {
+    const { stdout } = await execa(command);
+    return JSON.parse(stdout) as RGB;
+  } catch (error) {
+    if ((error as ExecaError).stdout === "No color selected") {
+      return undefined;
+    } else {
+      throw error;
+    }
+  }
+}
+
+/**
+ * Naive implementation of finding the nearest color in a list of rgb-colors
+ */
+export function nearestColor(color: RGB, colors: RGB[]) {
+  let minDistance = Infinity;
+  let nearestColor = colors[0];
+  if (!nearestColor) throw new Error("No colors provided");
+
+  for (const c of colors) {
+    const r = c.r - color.r;
+    const g = c.g - color.g;
+    const b = c.b - color.b;
+    const distance = r * r + g * g + b * b;
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      nearestColor = c;
+    }
+  }
+
+  return nearestColor;
+}
+
+export function toRgb(hex: string): RGB {
+  if (hex[0] === "#") hex = hex.slice(1);
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+  return { r, g, b };
+}
+
+export function toHex(rgb: RGB) {
+  const { r, g, b } = rgb;
+  return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+}
+
+export function toTailwind(color: RGB) {
+  const tailwindColors = getTransformedTailwindConfig();
+  const colors = Array.from(tailwindColors.keys());
+
+  const nearest = nearestColor(color, colors);
+  const tailwind = tailwindColors.get(nearest);
+
+  if (!tailwind) throw new Error("Could not find tailwind color");
+
+  return tailwind;
+}
